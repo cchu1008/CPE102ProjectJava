ba248e9f121739501754b6294170be50f54e6947
cchu1008 <cchu.1008@gmail.com>
2015-04-23 23:19:34 -0700

    Did the initial changing to java from Python. Still need to work out logistics, syntax, ext. -C

--- entities.java
+++ entities.java
@@ -138,13 +138,13 @@ public class Miner
 	private String name;
 	private int resource_limit;
 	private Point position;
-	private double rate;
+	private int rate;
 	private int[] imgs;
 	private double animation_rate;
 	private int current_img;
 	private int resource_count;
 	
-   public Miner(this, String name, int resource_limit, Point position, double rate, int[] imgs, double animation_rate)
+   public Miner(this, String name, int resource_limit, Point position, int rate, int[] imgs, double animation_rate)
    {
       super(name, position, imgs);
       this.resource_limit = resource_limit;
@@ -185,14 +185,14 @@ public class MinerNotFull
 	private String name;
 	private int resource_limit;
 	private Point position;
-	private double rate;
+	private int rate;
 	private int[] imgs;
 	private double animation_rate;
 	private int current_img;
 	private int resource_count;
 	
    public MinerNotFull(this, String name, int resource_limit, Point position, 
-      double rate, int[] imgs, double animation_rate)
+      int rate, int[] imgs, double animation_rate)
    {
       super(name, resource_limit, position, rate, imgs, animation_rate);
       this.resource_count = 0;
@@ -269,14 +269,14 @@ public class MinerFull
 	private String name;
 	private int resource_limit;
 	private Point position;
-	private double rate;
+	private int rate;
 	private int[] imgs;
 	private int current_img;
 	private double animation_rate;
 	private int resource_count;
 	
    public MinerFull(this, String name, int resource_limit, Point position, 
-      double rate, int[] imgs, double animation_rate)
+      int rate, int[] imgs, double animation_rate)
    {
       super(name, resource_limit, position, rate, imgs, animation_rate);
       this.resource_count = 2;
@@ -348,230 +348,414 @@ public class MinerFull
 }
 
 
-public class Vein(Pending_Actions):
-   def __init__(this, name, rate, position, imgs, resource_distance=1):
-      super(Vein, this).__init__(name, position, imgs)
-      this.rate = rate
-      this.resource_distance = resource_distance
+public class Vein
+   extends Pending_Actions
+{
+	private String name;
+	private int rate;
+	private Point position;
+	private int[] imgs;
+	private int current_img;
+	private int resource_distance;
+	
+   public Vein(this, String name, int rate, Point position, int[] imgs, 
+      int resource_distance=1)
+   {
+      super(name, position, imgs;
+      this.rate = rate;
+      this.resource_distance = resource_distance;
+   }
 
-   def get_rate(this):
-      return this.rate
+   public double get_rate()
+   {
+      return this.rate;
+   }
 
-   def get_resource_distance(this):
-      return this.resource_distance
+   public int get_resource_distance()
+   {
+      return this.resource_distance;
+   }
 
-   def entity_string(this):
+   public String entity_string()
+   {
       return ' '.join(['vein', this.name, str(this.position.x),
          str(this.position.y), str(this.rate),
-         str(this.resource_distance)])
+         str(this.resource_distance)]);
+   }
          
-   def create_vein_action(this, world, i_store):
-      def action(current_ticks):
-         this.remove_pending_action(action)
+   public Actions create_vein_action(World world, ***int[] i_store)
+   {
+	   private int current_ticks;
+	   private Point open_pt;
+	   private Entity ore;
+	   private Point[] tiles;
+	   
+      private int action(int current_ticks)
+	  {
+         this.remove_pending_action(action);
 
          open_pt = find_open_around(world, this.position,
-            this.resource_distance)
-         if open_pt:
+            this.resource_distance);
+         if (open_pt)
+		 {
             ore = actions.create_ore(world,
                "ore - " + this.name + " - " + str(current_ticks),
-               open_pt, current_ticks, i_store)
-            world.add_entity(ore)
-            tiles = [open_pt]
-         else:
-            tiles = []
+               open_pt, current_ticks, i_store);
+            world.add_entity(ore);
+            tiles = [open_pt];
+         else
+		 {
+            tiles = new Point[];
+		 }
 
          actions.schedule_action(world, this,
             this.create_vein_action(world, i_store),
-            current_ticks + this.rate)
-         return tiles
-      return action
+            current_ticks + this.rate);
+         return tiles;
+		 }
+      return action;
+	  }
+   }
+}
       
-public class Ore(Pending_Actions):
-   def __init__(this, name, position, imgs, rate=5000):
-      super(Ore, this).__init__(name, position, imgs)
-      this.rate = rate
+public class Ore
+   extends Pending_Actions
+{
+	private String name;
+	private Point position;
+	private int[] imgs;
+	private int current_img;
+	private int rate;
+	
+   public Ore(this, String name, Point position, int[] imgs, int rate=5000)
+   {
+      super(name, position, imgs);
+      this.rate = rate;
+   }
 
-   def get_rate(this):
-      return this.rate
+   public double get_rate()
+   {
+      return this.rate;
+   }
 
-   def entity_string(this):
+   public String entity_string()
+   {
       return ' '.join(['ore', this.name, str(this.position.x),
-         str(this.position.y), str(this.rate)])
+         str(this.position.y), str(this.rate)]);
+   }
+}
       
-public class Blacksmith(Pending_Actions):
-   def __init__(this, name, position, imgs, resource_limit, rate,
-      resource_distance=1):
-      super(Blacksmith, this).__init__(name, position, imgs)
-      this.resource_limit = resource_limit
-      this.resource_count = 0
-      this.rate = rate
-      this.resource_distance = resource_distance
+public class Blacksmith
+   extends Pending_Actions
+{
+	private String name;
+	private Point position;
+	private int[] imgs;
+	private int current_img;
+	private int resource_limit;
+	private int rate;
+	private resource_distance;
+	private int resource_count;
+	
+   public Blacksmith(this, String name, Point position, int[] imgs, 
+      int resource_limit, int rate, int resource_distance=1)
+   {
+      super(name, position, imgs);
+      this.resource_limit = resource_limit;
+      this.resource_count = 0;
+      this.rate = rate;
+      this.resource_distance = resource_distance;
+   }
 
-   def get_rate(this):
-      return this.rate
+   public double get_rate()
+   {
+      return this.rate;
+   }
 
-   def get_resource_distance(this):
-      return this.resource_distance
+   public int get_resource_distance()
+   {
+      return this.resource_distance;
+   }
       
-   def get_resource_count(this):
-      return this.resource_count
+   public int get_resource_count()
+   {
+      return this.resource_count;
+   }
 
-   def set_resource_count(this, n):
-      this.resource_count = n
+   public void set_resource_count(int n)
+   {
+      this.resource_count = n;
+   }
       
-   def get_resource_limit(this):
-      return this.resource_limit
+   public int get_resource_limit()
+   {
+      return this.resource_limit;
+   }
 
-   def entity_string(this):
+   public String entity_string()
+   {
       return ' '.join(['blacksmith', this.name, str(this.position.x),
          str(this.position.y), str(this.resource_limit),
-         str(this.rate), str(this.resource_distance)])
+         str(this.rate), str(this.resource_distance)]);
+   }
+}
 
-public class Obstacle(Entity):
-   def __init__(this, name, position, imgs):
-      super(Obstacle, this).__init__(name, position, imgs)
+public class Obstacle
+   extends Entity
+{
+	private String name;
+	private Point position;
+	private int[] imgs;
+	private int current_img;
+	
+   public Obstacle(this, String name, Point position, int[] imgs)
+   {
+      super(name, position, imgs);
+   }
 
-   def entity_string(this):
+   public String entity_string()
+   {
       return ' '.join(['obstacle', this.name, str(this.position.x),
-         str(this.position.y)])
+         str(this.position.y)]);
+   }
+}
 
-public class OreBlob(Pending_Actions):
-   def __init__(this, name, position, rate, imgs, animation_rate):
-      super(OreBlob, this).__init__(name, position, imgs)
-      this.rate = rate
-      this.animation_rate = animation_rate
+public class OreBlob
+   extends Pending_Actions
+{
+	private String name;
+	private Point position;
+	private int rate;
+	private int[] imgs;
+	private int current_img;
+	private double animation_rate;
+	
+   public OreBlob(this, String name, Point position, int rate, int[] imgs, 
+      double animation_rate)
+   {
+      super(name, position, imgs);
+      this.rate = rate;
+      this.animation_rate = animation_rate;
+   }
 
-   def get_rate(this):
-      return this.rate
+   public double get_rate()
+   {
+      return this.rate;
+   }
 
-   def get_animation_rate(this):
-      return this.animation_rate
+   public double get_animation_rate()
+   {
+      return this.animation_rate;
+   }
 
-   def entity_string(this):
-      return 'unknown'
+   public String entity_string()
+   {
+      return 'unknown';
+   }
       
-   def blob_next_position(this, world, dest_pt):
-      horiz = sign(dest_pt.x - this.position.x)
-      new_pt = point.Point(this.position.x + horiz, this.position.y)
+   public Point blob_next_position(World world, Point dest_pt)
+   {
+      ***horiz = sign(dest_pt.x - this.position.x)
+      Point new_pt = new Point(this.position.x + horiz, this.position.y);
 
-      if horiz == 0 or (world.is_occupied(new_pt) and
-         not isinstance(world.get_tile_occupant(new_pt), Ore)):
-         vert = sign(dest_pt.y - this.position.y)
-         new_pt = point.Point(this.position.x, this.position.y + vert)
+      if (horiz == 0 || (world.is_occupied(new_pt) &&
+         !isinstance(world.get_tile_occupant(new_pt), Ore))
+	  {
+         ***vert = sign(dest_pt.y - this.position.y);
+         new_pt = new Point(this.position.x, this.position.y + vert);
 
-         if vert == 0 or (world.is_occupied(new_pt) and
-            not isinstance(world.get_tile_occupant(new_pt), Ore)):
-            new_pt = point.Point(this.position.x, this.position.y)
+         if (vert == 0 || (world.is_occupied(new_pt) &&
+            !isinstance(world.get_tile_occupant(new_pt), Ore))
+	     {
+            new_pt = new Point(this.position.x, this.position.y);
+		 }
+	  }
 
-      return new_pt
+      return new_pt;
+   }
       
-   def blob_to_vein(this, world, vein):
-      entity_pt = this.position
-      if not vein:
-         return ([entity_pt], False)
-      vein_pt = vein.get_position()
-      if adjacent(entity_pt, vein_pt):
-         actions.remove_entity(world, vein)
-         return ([vein_pt], True)
-      else:
-         new_pt = this.blob_next_position(world, vein_pt)
-         old_entity = world.get_tile_occupant(new_pt)
-         if isinstance(old_entity, Ore):
-            actions.remove_entity(world, old_entity)
-         return (world.move_entity(this, new_pt), False)
+***public boolean blob_to_vein(World world, Entity vein)
+   {
+      Point entity_pt = this.position;
+      if (!vein)
+	  {
+         return ([entity_pt], False);
+	  }
+      Point vein_pt = vein.get_position();
+      if (adjacent(entity_pt, vein_pt))
+	  {
+         actions.remove_entity(world, vein);
+         return ([vein_pt], True);
+	  }
+      else
+	  {
+         Point new_pt = this.blob_next_position(world, vein_pt);
+         Entity old_entity = world.get_tile_occupant(new_pt);
+         if (isinstance(old_entity, Ore))
+		 {
+            actions.remove_entity(world, old_entity);
+		 }
+         return (world.move_entity(this, new_pt), False);
+	  }
+   }
          
-   def create_ore_blob_action(this, world, i_store):
-      def action(current_ticks):
-         this.remove_pending_action(action)
+   public Actions create_ore_blob_action(World world, ***int[] i_store)
+   {
+	   private int current_ticks;
+	   private Point entity_pt;
+	   private Entity vein;
+	   private int next_time;
+	   private Entity quake;
+	   
+	   
+      private int action(int current_ticks)
+	  {
+         this.remove_pending_action(action);
 
-         entity_pt = this.position
-         vein = world.find_nearest(entity_pt, Vein)
-         (tiles, found) = this.blob_to_vein(world, vein)
+         entity_pt = this.position;
+         vein = world.find_nearest(entity_pt, Vein);
+         (tiles, found) = this.blob_to_vein(world, vein);
 
-         next_time = current_ticks + this.rate
-         if found:
-            quake = actions.create_quake(world, tiles[0], current_ticks, i_store)
-            world.add_entity(quake)
-            next_time = current_ticks + this.rate * 2
+         next_time = current_ticks + this.rate;
+         if (found)
+		 {
+            quake = actions.create_quake(world, tiles[0], current_ticks, i_store);
+            world.add_entity(quake);
+            next_time = current_ticks + this.rate * 2;
+		 }
 
          actions.schedule_action(world, this,
             this.create_ore_blob_action(world, i_store),
-            next_time)
+            next_time);
 
-         return tiles
-      return action
+         return tiles;
+	  }
+      return action;
+   }
+}
       
-public class Quake(Pending_Actions):
-   def __init__(this, name, position, imgs, animation_rate):
-      super(Quake, this).__init__(name, position, imgs)
-      this.animation_rate = animation_rate
+public class Quake
+   extends Pending_Actions
+{
+	private String name;
+	private Point position;
+	private int[] imgs;
+	private int current_img;
+	private double animation_rate;
+	
+   public Quake(this, String name, Point position, int[] imgs, double animation_rate)
+   {
+      super(name, position, imgs);
+      this.animation_rate = animation_rate;
+   }
 
-   def get_animation_rate(this):
-      return this.animation_rate
+   public double get_animation_rate()
+   {
+      return this.animation_rate;
+   }
 
-   def entity_string(this):
-      return 'unknown'
+   public String entity_string()
+   {
+      return 'unknown';
+   }
+}
       
       
-def try_transform_miner_full(world, entity):
-   new_entity = MinerNotFull(
+public Entity try_transform_miner_full(World world, Entity entity)
+{
+   Entity new_entity = MinerNotFull(
       entity.get_name(), entity.get_resource_limit(),
       entity.get_position(), entity.get_rate(),
-      entity.get_images(), entity.get_animation_rate())
+      entity.get_images(), entity.get_animation_rate());
 
-   return new_entity
+   return new_entity;
+}
       
-def try_transform_miner_not_full(world, entity):
-   if entity.resource_count < entity.resource_limit:
-      return entity
-   else:
-      new_entity = MinerFull(
+public Entity try_transform_miner_not_full(World world, Entity entity)
+{
+   if (entity.resource_count < entity.resource_limit)
+   {
+      return entity;
+   }
+   else
+   {
+      Entity new_entity = MinerFull(
          entity.get_name(), entity.get_resource_limit(), entity.get_position(),
-         entity.get_rate(), entity.get_images(), entity.get_animation_rate())
-      return new_entity
+         entity.get_rate(), entity.get_images(), entity.get_animation_rate());
+      return new_entity;
+   }
+}
       
-def try_transform_miner(world, entity, transform):
-   new_entity = transform(world, entity)
-   if entity != new_entity:
-      entity.clear_pending_actions(world)
-      world.remove_entity_at(entity.get_position())
-      world.add_entity(new_entity)
-      actions.schedule_animation(world, new_entity)
+public Entity try_transform_miner(World world, Entity entity, ***transform)
+{
+   Entity new_entity = transform(world, entity);
+   if (entity != new_entity)
+   {
+      entity.clear_pending_actions(world);
+      world.remove_entity_at(entity.get_position());
+      world.add_entity(new_entity);
+      ***actions.schedule_animation(world, new_entity);
+   }
 
-   return new_entity
+   return new_entity;
+}
 
-def adjacent(pt1, pt2):
-   return ((pt1.x == pt2.x and abs(pt1.y - pt2.y) == 1) or
-      (pt1.y == pt2.y and abs(pt1.x - pt2.x) == 1))
+public boolean adjacent(pt1, pt2)
+{
+   return ((pt1.x == pt2.x && abs(pt1.y - pt2.y) == 1) ||
+      (pt1.y == pt2.y && abs(pt1.x - pt2.x) == 1));
+}
 
-def sign(x):
-   if x < 0:
-      return -1
-   elif x > 0:
-      return 1
-   else:
-      return 0
+public int sign(x)
+{
+   if (x < 0)
+   {
+      return -1;
+   }
+   else if (x > 0)
+   {
+      return 1;
+   }
+   else
+   {
+      return 0;
+   }
+}
 
-def next_position(world, entity_pt, dest_pt):
-   horiz = sign(dest_pt.x - entity_pt.x)
-   new_pt = point.Point(entity_pt.x + horiz, entity_pt.y)
+public Point next_position(World world, Point entity_pt, Point dest_pt)
+{
+   int horiz = sign(dest_pt.x - entity_pt.x);
+   Point new_pt = new CartesianPoint(entity_pt.x + horiz, entity_pt.y);
 
-   if horiz == 0 or world.is_occupied(new_pt):
-      vert = sign(dest_pt.y - entity_pt.y)
-      new_pt = point.Point(entity_pt.x, entity_pt.y + vert)
+   if (horiz == 0 || world.is_occupied(new_pt))
+   {
+      int vert = sign(dest_pt.y - entity_pt.y);
+      new_pt = new CartesianPoint(entity_pt.x, entity_pt.y + vert);
 
-      if vert == 0 or world.is_occupied(new_pt):
-         new_pt = point.Point(entity_pt.x, entity_pt.y)
+      if (vert == 0 || world.is_occupied(new_pt))
+	  {
+         new_pt = new CartesianPoint(entity_pt.x, entity_pt.y);
+	  }
+   }
 
-   return new_pt
+   return new_pt;
+}
    
-def find_open_around(world, pt, distance):
-   for dy in range(-distance, distance + 1):
-      for dx in range(-distance, distance + 1):
-         new_pt = point.Point(pt.x + dx, pt.y + dy)
+public Point find_open_around(World world, Point pt, int distance)
+{
+   for (dy ***in range(-distance, distance + 1))
+   {
+      for (dx ***in range(-distance, distance + 1))
+	  {
+         Point new_pt = new CartesianPoint(pt.x + dx, pt.y + dy);
 
-         if (world.within_bounds(new_pt) and
-            (not world.is_occupied(new_pt))):
-            return new_pt
+         if ((world.within_bounds(new_pt) &&
+            (!(world.is_occupied(new_pt)))))
+		 {
+            return new_pt;
+		 }
+	  }
+   }
 
-   return None
+   return None;
+}
