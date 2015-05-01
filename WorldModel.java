ab33b8dd28a0d3567ab9cb8c516d7b52a9f160d4
HarriKnox <harri.alpha96@gmail.com>
2015-05-01 01:24:29 -0700

    Added a couple more World methods

--- WorldModel.java
+++ WorldModel.java
@@ -1,18 +1,21 @@
+import java.lang.Math;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Comparator;
 
 public class WorldModel
 {
 	private int width;
 	private int height;
-	private Entity[][] occupancy;
+	private Entity[][] occupancies;
 	private List<Entity> entities;
 	
 	public WorldModel(int width, int height)
 	{
 		this.width = width;
 		this.height = height;
-		this.occupancy = new Entity[width][height];
+		this.occupancies = new Entity[width][height];
 		this.entities = new ArrayList<Entity>();
 	}
 	
@@ -26,11 +29,111 @@ public class WorldModel
 		return this.height;
 	}
 	
+	public List getEntities()
+	{
+		return this.entities;
+	}
+	
+	public boolean isOccupied(Point position)
+	{
+		return this.withinBounds(position) && (this.getCell(position) != null);
+	}
+	
+	private Entity getCell(Point p)
+	{
+		if (this.withinBounds(p))
+		{
+			return this.occupancies[p.getXCoord()][p.getYCoord()];
+		}
+		return null;
+	}
+	
+	private void setCell(Point p, Entity ent)
+	{
+		if (this.withinBounds(p))
+		{
+			this.occupancies[p.getXCoord()][p.getYCoord()] = ent;
+		}
+	}
+	
 	public boolean withinBounds(Point position)
 	{
 		int x = position.getXCoord();
 		int y = position.getYCoord();
 		return x >= 0 && x < this.width && y >= 0 && y < this.height;
+	}
+	
+	public void addEntity(Entity ent)
+	{
+		Point pos = ent.getPosition();
+		if (this.withinBounds(pos))
+		{
+			Entity old = this.getCell(pos);
+			if (old != null)
+			{
+				this.entities.remove(old);
+			}
+			this.setCell(pos, ent);
+			this.entities.add(ent);
+		}
+	}
+	
+	public void moveEntity(Entity ent, Point newPoint)
+	{
+		Point pos = ent.getPosition();
+		if (this.withinBounds(newPoint) && this.getCell(pos) == ent)
+		{
+			this.setCell(pos, null);
+			this.setCell(newPoint, ent);
+			ent.setPosition(newPoint);
+		}
+	}
+	
+	public void removeEntity(Entity ent)
+	{
+		this.removeEntityAt(ent.getPosition());
+	}
+	
+	public void removeEntityAt(Point pos)
+	{
+		Entity old = this.getCell(pos);
+		if (this.withinBounds(pos) && old != null)
+		{
+			this.setCell(pos, null);
+			this.entities.remove(old);
+			old.setPosition(Point.NegativePoint);
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	public Entity findNearest(Point pos, Class type)
+	{
+		if (this.entities.size() == 0)
+			return null;
+			
+		Entity closestEntity = this.entities.get(0);
+		double closestDistance = this.distance(closestEntity.getPosition(), pos);
+		for (int i = 1; i < this.entities.size(); i++)
+		{
+			Entity ent = this.entities.get(i);
+			if (type.isAssignableFrom(ent.getClass()))
+			{
+				double dist = this.distance(ent.getPosition(), pos);
+				if (dist < closestDistance)
+				{
+					closestEntity = ent;
+					closestDistance = dist;
+				}
+			}
+		}
+		return closestEntity;
+	}
+	
+	private static double distance(Point pos1, Point pos2)
+	{
+		int dx = pos1.getXCoord() - pos2.getXCoord();
+		int dy = pos1.getYCoord() - pos2.getYCoord();
+		return Math.sqrt((double)((dx * dx) + (dy * dy)));
 	}
 }
 
