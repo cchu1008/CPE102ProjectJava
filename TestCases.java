bc424a9b00e6767a49e8545a6a82aff562f8cf81
HarriKnox <harri.alpha96@gmail.com>
2015-05-01 22:04:00 -0700

    Added more test cases

--- TestCases.java
+++ TestCases.java
@@ -7,13 +7,34 @@ import org.junit.Before;
 
 public class TestCases
 {
-	private static final Background back = new Background();
-	private static final WorldModel theWorld = new WorldModel(16, 16, back);
+	private static final Point point = new Point(10, 16);
+	
 	private static final Miner Harrison = new Miner(new Point(1, 11), 2, 0);
 	private static final Miner Courtney = new Miner(new Point(10, 8), 2, 2);
 	
+	private static final Background back = new Background();
+	
+	private static final WorldModel theWorld = new WorldModel(16, 16, back);
+	
+	private static final Blacksmith smithy = new Blacksmith(new Point(3, 8), 10, 3, 1);
+	
+	private static final Obstacle obby = new Obstacle(new Point(4, 4));
+	
+	private static final OreBlob blobby;
+	
+	private static final Ore oreby;
+	
+	private static final Vein veiny;
+	
+	@test
+	public void testPoint()
+	{
+		assertEqual(10, point.getXCoord());
+		assertEqual(16, point.getYCoord());
+	}
+	
 	@Test
-	public void testEntity()
+	public void testMiners()
 	{
 		
 		assertEquals(10, Courtney.getPosition().getXCoord());
@@ -43,4 +64,29 @@ public class TestCases
 		assertEquals("unknown", Harrison.entityString(false));
 		assertEquals("miner miner_10_8 10 8 2", Courtney.entityString(false));
 	}
+	
+	@test
+	public void testBlacksmith()
+	{
+		assertEquals(3, smithy.getPosition().getXCoord());
+		assertEquals(8, smithy.getPosition().getYCoord());
+		
+		assertEquals(10, smithy.getResourceLimit());
+		assertEquals(3, smithy.getResourceCount());
+		assertEquals(1, smithy.getResourceDistance());
+		
+		smithy.incrementResourceCount();
+		assertEquals(4, smithy.getResourceCount());
+		for (int i = 4; i < 10; i++)
+		{
+			smithy.incrementResourceCount();
+		}
+		assertTrue(smithy.isFull());
+		
+		smithy.setResourceCount(7);
+		assertFalse(smithy.isFull());
+		assertEquals(7, smithy.getResourceCount());
+	}
+	
+	
 }
