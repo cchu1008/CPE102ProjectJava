70f4bc3fdd9b70b96bd34b0ea3d04eba2aa62214
HarriKnox <harri.alpha96@gmail.com>
2015-05-17 22:51:14 -0700

    Removed resource stuff in Blacksmith cos apparently it was never there to begin with.

--- Blacksmith.java
+++ Blacksmith.java
@@ -2,44 +2,7 @@ import processing.core.*;
 
 public class Blacksmith extends Entity
 {
-	private int resourceLimit;
-	private int resourceCount;
-	
-	public Blacksmith(Point pos, PImage img, int rLim, int rCount)
+	public Blacksmith(Point pos, PImage img)
 	{
 		super("blacksmith", pos, img);
-		this.resourceLimit = rLim;
-		this.resourceCount = rCount;
 	}
-	
-	public int getResourceLimit()
-	{
-		return this.resourceLimit;
-	}
-	
-	public int getResourceCount()
-	{
-		return this.resourceCount;
-	}
-	
-	public void incrementResourceCount()
-	{
-		this.resourceCount += 1;
-	}
-	
-	public void setResourceCount(int num)
-	{
-		this.resourceCount = num;
-	}
-	
-	public boolean isFull()
-	{
-		return this.resourceCount >= this.resourceLimit;
-	}
-	
-	public String entityString()
-	{
-		String initial = super.entityString();
-		return String.format("%s %d %d", initial, this.getResourceLimit(), this.getResourceCount());
-	}
-}
