c25c9495d4fd0d9badcfa1a6e73a4cc8c417e3e0
cchu1008 <cchu.1008@gmail.com>
2015-06-03 14:51:58 -0700

    Started Birdie! -C

---
+++ Birdie.java
@@ -0,0 +1,9 @@
+import processing.core.*;
+
+public class Birdie extends Animated
+{
+	public Birdie(String name, Point position, List<PImage> imageList, int actRate, int animRate)
+	{
+		super(name, position, imageList, actRate, animRate);
+	}
+}
